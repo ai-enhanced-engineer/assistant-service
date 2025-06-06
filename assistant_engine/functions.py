ad7016a7f7f472980fe83fec18312816eb962563
Leopoldo Garcia Vargas <54072655+lkronecker13@users.noreply.github.com>
2025-06-06 01:31:13 -0400

    fix imports and formatting

--- assistant_engine/functions.py
+++ assistant_engine/functions.py
@@ -4,6 +4,7 @@ was activated for a specific assistant. The variable TOOL_MAP bellow is a key va
 is used as a string key and the value is a programmatic reference to the function to be invoked. See examples in:
 'assistant_factory/client_spec/{CLIENT_ID}/functions.py'
 """
+
 from types import FunctionType
 
 TOOL_MAP: dict[str:FunctionType] = {}
