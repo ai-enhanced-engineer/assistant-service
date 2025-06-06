6c48f5f609d7901be347a445dd77c1b20ca20e76
Leopoldo Garcia Vargas <54072655+lkronecker13@users.noreply.github.com>
2025-06-06 01:49:33 -0400

    Fix tests and add semantic-release workflow

--- assistant_factory/create_assistant.py
+++ assistant_factory/create_assistant.py
@@ -4,14 +4,17 @@ from dotenv import load_dotenv
 from openai import AsyncOpenAI
 from openai.types.beta import Assistant
 
-load_dotenv()
 
-api_key = os.environ.get("OPENAI_API_KEY")
-client = AsyncOpenAI(api_key=api_key)
+def _get_client() -> AsyncOpenAI:
+    """Create an AsyncOpenAI client using env configuration."""
+    load_dotenv()
+    api_key = os.environ.get("OPENAI_API_KEY")
+    return AsyncOpenAI(api_key=api_key)
 
 
 async def create_assistant(name: str, instructions: str, a_tools: list[dict], model: str, file_ids: list) -> Assistant:
     print("Creating assistant!")
+    client = _get_client()
     return await client.beta.assistants.create(
         name=name,
         instructions=instructions,
@@ -23,5 +26,6 @@ async def create_assistant(name: str, instructions: str, a_tools: list[dict], mo
 
 async def upload_files_for_retrieval(path_to_file: str):
     # Todo: Make this upload a list of files
+    client = _get_client()
     file = await client.files.create(file=open(path_to_file, "rb"), purpose="assistants")
     return file.id
--- assistant_factory/tests/test_main.py
+++ assistant_factory/tests/test_main.py
@@ -1,7 +1,6 @@
-import pytest
 
-from assistant_factory.main import persist_config
 from assistant_factory.client_spec.leogv.assistants import ClientAssistantConfig
+from assistant_factory.main import persist_config
 from botbrew_commons.data_models import EngineAssistantConfig
 
 
